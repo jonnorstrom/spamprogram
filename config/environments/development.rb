179d1e79e978c5653faed6a4cb1f398ed36e8be5
Jonathan Norstrom <jon.norstrom@gmail.com>
2018-03-19 09:55:20 -0400

    working mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,9 +27,21 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:                'smtp.gmail.com',
+    port:                   587,
+    domain:                 'gmail.com',
+    user_name:              "#{ENV['gmail_username']}@gmail.com",
+    password:               "#{ENV['gmail_password']}",
+    authentication:         'plain',
+    enable_starttls_auto:   true
+  }
+  ### NOTE: do NOT user an email account w/ 2-step authentication
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
